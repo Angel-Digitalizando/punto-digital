30df0c462b683b0db9f8e81adf06e2703da19318
Angel-Digitalizando <angel.digitalizando@proton.me>
2026-06-06 02:48:07 -0300

    servicios

--- js/tutoriales.js
+++ js/tutoriales.js
@@ -400,14 +400,15 @@ const baseDeTutoriales = {
 
     servicios: {
         categoria: 'comunidad',
-        titulo: "Servicios del Barrio y Trámites",
-        icono: "🚌",
-        detalle: "Accedé directo a las herramientas que necesitás para moverte y gestionar tus papeles sin vueltas.",
+        nivel:     'intermedio'
+        titulo:    "Servicios del Barrio y Trámites",
+        icono:     "🚌",
+        detalle:   "Accedé directo a las herramientas que necesitás para moverte y gestionar tus papeles sin vueltas.",
         pasos: [
-             "<button class='btn-paso' onclick=\"window.open('https://www.laperlita.com.ar/','_blank')\">🚌 La Perlita (Horarios y recorridos)</button>",
-             "<button class='btn-paso' onclick=\"window.open('https://mi.argentina.gob.ar/','_blank')\">🇦🇷 Mi Argentina (DNI, trámites y credenciales)</button>",    
-             "<button class='btn-paso' onclick=\"window.open('https://www.anses.gob.ar/','_blank')\">🏛️ ANSES (Jubilaciones y Asignaciones)</button>",
-             "<button class='btn-paso' onclick=\"window.open('http://www.moreno.gob.ar/','_blank')\">🏠 Municipio de Moreno (Servicios Locales)</button>",
+            "<button class='btn-paso' onclick=\"window.open('https://www.laperlita.com.ar/','_blank')\">🚌 La Perlita (Horarios y recorridos)</button>",
+            "<button class='btn-paso' onclick=\"window.open('https://mi.argentina.gob.ar/','_blank')\">🇦🇷 Mi Argentina (DNI, trámites y credenciales)</button>",    
+            "<button class='btn-paso' onclick=\"window.open('https://www.anses.gob.ar/','_blank')\">🏛️ ANSES (Jubilaciones y Asignaciones)</button>",
+            "<button class='btn-paso' onclick=\"window.open('http://www.moreno.gob.ar/','_blank')\">🏠 Municipio de Moreno (Servicios Locales)</button>",
         ],
         nota: "💡 Estos enlaces te llevan a las páginas oficiales. Recordá nunca compartir tus claves personales en lugares públicos."
     }
@@ -415,6 +416,3 @@ const baseDeTutoriales = {
 // FUERZA LA EXPOSICIÓN GLOBAL (REQUERIDO POR UI.JS)
  window.baseDeTutoriales = baseDeTutoriales;
  console.log("Tutoriales cargados correctamente en window:", window.baseDeTutoriales);
-
-
- 
